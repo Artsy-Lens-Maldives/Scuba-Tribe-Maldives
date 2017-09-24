9789765a464a6c14a14a4a896986cda50baddf14
athik13 <athik.13@gmail.com>
2017-09-24 20:15:50 +0500

    improved subscribe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,13 +16,13 @@
           </ul>
         </div>
         <!-- call out section-->
-      <section class="page-section pt-90 pb-80 bg-main pattern relative">
+      <section class="page-section-sub pt-90 pb-80 bg-main pattern relative">
         <div class="container">
           <div class="call-out-box clearfix with-icon">
             <div class="row call-out-wrap">
               <div class="col-md-5">
                 <h6 class="title-section-top gray font-4">subscribe today</h6>
-                <h2 class="title-section alt-2"><span>Get</span> Latest offers</h2><i class="flaticon-suntour-email call-out-icon"></i>
+                <h2 class="title-section alt-2"><span>Get</span> Latest offers</h2>
               </div>
               <div class="col-md-7">
                 <form action="php/contacts-process.php" method="post" class="form contact-form mt-10">

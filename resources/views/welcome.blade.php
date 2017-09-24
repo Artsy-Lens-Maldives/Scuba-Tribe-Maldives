b1fd7e21df3718b79985c87e1496e6cbc7055f78
Yaniu09 <imperialaeiou@gmail.com>
2017-09-24 08:09:51 -0700

    welcome page edit still

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,100 +38,73 @@
       </section>
       <!-- ! call out section-->
       </div>
+
       <!-- gallery section-->
       <section class="small-section">
         <div class="container">
           <div class="row">
             <div class="col-md-8">
-              <h6 class="title-section-top font-4">Happy Memories</h6>
-              <h2 class="title-section"><span>Photo</span> Gallery</h2>
+              <h6 class="title-section-top font-4"></h6>
+              <h2 class="title-section"><span>About</span> Us</h2>
               <div class="cws_divider mb-25 mt-5"></div>
-              <p>Vestibulum feugiat vitae tortor ut venenatis. Sed cursus, purus eu euismod bibendum, diam nisl suscipit odio, vitae ultrices mauris dolor quis mauris. Curabitur ac metus id leo maximus porta.</p>
+              <p>Read about our fantastic organization which provides varaities of services which is unmatchable to any other in this industry. </p>
             </div>
             <div class="col-md-4"><i class="flaticon-suntour-photo title-icon"></i></div>
           </div>
-          <div class="row portfolio-grid">
-            <!-- portfolio item-->
-            <div class="col-md-6 col-sm-12 col-xs-12">
-              <div class="portfolio-item big">
-                <!-- portfolio image--><a href="pic/portfolio/580x285-1@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/580x285-1.jpg" data-at2x="pic/portfolio/580x285-1@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/580x285-1@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
+          <!-- tabs-->
+            <div class="col-md-12 mt-md-40">
+              <div class="tabs">
+                <div class="block-tabs-btn clearfix">
+                  <div data-tabs-id="tabs1" class="tabs-btn active">About us</div>
+                  <div data-tabs-id="tabs2" class="tabs-btn">Our mission</div>
+                  <div data-tabs-id="tabs3" class="tabs-btn">Our vision</div>
+                </div>
+                <!-- tabs keeper-->
+                <div class="tabs-keeper">
+                  <!-- tabs container-->
+                  <div data-tabs-id="cont-tabs1" class="container-tabs active">
+                    <h6 class="trans-uppercase">Hotel Bohemians</h6>
+                    <p>Duis egestas accumsan ipsum, at volutpat elit imperdiet in. Curabitur lacinia, massa quis elementum bibendum, tellus neque porttitor erat, a ornare enim arcu nec mauris. Morbi ac tristique felis. Praesent cursus placerat risus. Duis ut magna quis sem varius consequat.  </p>
+                    <ul class="style-3">
+                      <li>Nam molestie dolor id auctor sodales;</li>
+                      <li>In sagittis dolor vel turpis aliquet pharetra;</li>
+                      <li>Quisque non turpis in dui congue dapibus;</li>
+                      <li>Vivamus varius nisl quis dictum maximus;</li>
+                      <li>Vestibulum scelerisque ligula quis est faucibus tincidunt.</li>
+                    </ul>
+                  </div>
+                  <!-- /tabs container-->
+                  <!-- tabs container-->
+                  <div data-tabs-id="cont-tabs2" class="container-tabs">
+                    <h6 class="trans-uppercase">Hotel Bohemians</h6>
+                    <p>Duis egestas accumsan ipsum, at volutpat elit imperdiet in. Curabitur lacinia, massa quis elementum bibendum, tellus neque porttitor erat, a ornare enim arcu nec mauris. Morbi ac tristique felis. Praesent cursus placerat risus. Duis ut magna quis sem varius consequat.  </p>
+                    <ul class="style-3">
+                      <li>Nam molestie dolor id auctor sodales;</li>
+                      <li>In sagittis dolor vel turpis aliquet pharetra;</li>
+                      <li>Quisque non turpis in dui congue dapibus;</li>
+                      <li>Vivamus varius nisl quis dictum maximus;</li>
+                      <li>Vestibulum scelerisque ligula quis est faucibus tincidunt.</li>
+                    </ul>
+                  </div>
+                  <!-- /tabs container-->
+                  <!-- tabs container-->
+                  <div data-tabs-id="cont-tabs3" class="container-tabs">
+                    <h6 class="trans-uppercase">Hotel Bohemians</h6>
+                    <p>Duis egestas accumsan ipsum, at volutpat elit imperdiet in. Curabitur lacinia, massa quis elementum bibendum, tellus neque porttitor erat, a ornare enim arcu nec mauris. Morbi ac tristique felis. Praesent cursus placerat risus. Duis ut magna quis sem varius consequat.  </p>
+                    <ul class="style-3">
+                      <li>Nam molestie dolor id auctor sodales;</li>
+                      <li>In sagittis dolor vel turpis aliquet pharetra;</li>
+                      <li>Quisque non turpis in dui congue dapibus;</li>
+                      <li>Vivamus varius nisl quis dictum maximus;</li>
+                      <li>Vestibulum scelerisque ligula quis est faucibus tincidunt.</li>
+                    </ul>
+                  </div>
+                  <!-- /tabs container-->
+                </div>
+                <!-- /tabs keeper-->
               </div>
             </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-1@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-1.jpg" data-at2x="pic/portfolio/285x285-1@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-1@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-2@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-2.jpg" data-at2x="pic/portfolio/285x285-2@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-2@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-3@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-3.jpg" data-at2x="pic/portfolio/285x285-3@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-3@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-4@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-4.jpg" data-at2x="pic/portfolio/285x285-4@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-4@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-5@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-5.jpg" data-at2x="pic/portfolio/285x285-5@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-5@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-6@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-6.jpg" data-at2x="pic/portfolio/285x285-6@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-6@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-7@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-7.jpg" data-at2x="pic/portfolio/285x285-7@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-7@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-3 col-sm-6 col-xs-6">
-              <div class="portfolio-item">
-                <!-- portfolio image--><a href="pic/portfolio/285x285-8@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/285x285-8.jpg" data-at2x="pic/portfolio/285x285-8@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/285x285-8@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-            <!-- portfolio item-->
-            <div class="col-md-6 col-sm-12 col-xs-12">
-              <div class="portfolio-item big">
-                <!-- portfolio image--><a href="pic/portfolio/580x285-2@2x.jpg" class="fancy">
-                  <div class="portfolio-media"><img src="pic/portfolio/580x285-2.jpg" data-at2x="pic/portfolio/580x285-2@2x.jpg" alt></div></a>
-                <div class="links"><a href="pic/portfolio/580x285-2@2x.jpg" class="fancy"><i class="fa fa-expand"></i></a></div>
-              </div>
-            </div>
-          </div>
+            <!-- /tabs-->
         </div>
       </section>
       <!-- ! gallery section-->
@@ -140,7 +113,7 @@
         <div class="container">
           <div class="row">
             <div class="col-md-8">
-              <h6 class="title-section-top font-4">Happy Memories</h6>
+              <h6 class="title-section-top font-4"></h6>
               <h2 class="title-section alt-2"><span>Our</span> Testimonials</h2>
               <div class="cws_divider mb-25 mt-5"></div>
             </div>

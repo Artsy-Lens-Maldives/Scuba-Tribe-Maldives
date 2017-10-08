b7458fcd66d52b729c0c9909d64655020a6d1867
athik13 <athik.13@gmail.com>
2017-10-08 21:18:30 +0500

    new inquery

--- resources/views/inquery.blade.php
+++ resources/views/inquery.blade.php
@@ -1,86 +1,59 @@
-   @extends('layouts.inquery')
+@extends('layouts.app')
 
 @section('content')
+<div class="content-body" style="margin-top: 100px">
+  <section class="page-section pt-0 pb-50">
+  <!-- review -->
+  <div class="container">
+    <div class="row" style="">
+      <div class="col-md-12">
+        <h4 class="trans-uppercase mb-10">Send an Inquery about (the fuck pacakge)</h4>
+        <div class="cws_divider mb-30"></div>
+      </div>
+    </div>
+    
+    <div class="review-content pattern relative">
+      
 
-   <!-- review -->
-        <div class="container">
-          <div class="row" style="">
-            <div class="col-md-12">
-              <h4 class="trans-uppercase mb-10">Send an Inquery about (the fuck pacakge)</h4>
-              <div class="cws_divider mb-30"></div>
+      <div class="row">
+        <div class="col-md-5 mb-md-30 mb-xs-0 mb-30">
+          <div class="review-total"><img src="/pic/blog/120x120.jpg" data-at2x="/pic/blog/120x120@2x.jpg" alt>
+            <div class="review-total-content">
+              
+              <ul class="icon">
+                <li>Maldives<i class="flaticon-suntour-map"></i></li>
+              </ul>
             </div>
-          </div>
-          
-          <div class="review-content pattern relative">
-            
-
-            <div class="row">
-              <div class="col-md-5 mb-md-30 mb-xs-0">
-                <div class="review-total"><img src="/pic/blog/120x120.jpg" data-at2x="/pic/blog/120x120@2x.jpg" alt>
-                  <div class="review-total-content">
-                    
-                    <ul class="icon">
-                      <li>Maldives<i class="flaticon-suntour-map"></i></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-7">
-                <div class="review-marks clearfix mb-30">
-                  <ul>
-                    <li>Cleanliness
-                      <div class="stars stars-5"></div>
-                    </li>
-                    <li>Location
-                      <div class="stars stars-5"></div>
-                    </li>
-                    <li>Staff
-                      <div class="stars stars-5"></div>
-                    </li>
-                    <li>Free Wi-Fi
-                      <div class="stars stars-5"></div>
-                    </li>
-                  </ul>
-                  <ul>
-                    <li>Comfort
-                      <div class="stars stars-5"></div>
-                    </li>
-                    <li>Facilities
-                      <div class="stars stars-5"></div>
-                    </li>
-                    <li>Value for money
-                      <div class="stars stars-5"></div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <form class="form clearfix">
-              <div class="row">
-                <div class="col-md-4">
-                  <input type="text" name="email" value="" size="40" placeholder="First Name" aria-required="true" class="form-row form-row-first">
-                </div>
-                <div class="col-md-4">
-                  <input type="text" name="email" value="" size="40" placeholder="Last Name" aria-required="true" class="form-row form-row-first">
-                </div>
-                <div class="col-md-4">
-                  <input type="text" name="email" value="" size="40" placeholder="Booking Number" aria-required="true" class="form-row form-row-first">
-                </div>
-                <div class="col-md-12">
-                  <input type="text" name="password" value="" size="40" placeholder="Title of your review" aria-required="true" class="form-row form-row-last">
-                </div>
-                <div class="col-md-12">
-                  <textarea name="message" cols="40" rows="4" placeholder="Message of your review" aria-invalid="false" aria-required="true" class="mb-20"></textarea>
-                  <input type="submit" value="Add a review" class="cws-button alt float-right">
-                </div>
-              </div>
-            </form>
-
-            
-
-                    
           </div>
         </div>
-        <!-- ! review -->
+      </div>
+      <form class="form clearfix">
+        <div class="row">
+          <div class="col-md-4">
+            <input type="text" name="email" value="" size="40" placeholder="First Name" aria-required="true" class="form-row form-row-first">
+          </div>
+          <div class="col-md-4">
+            <input type="text" name="email" value="" size="40" placeholder="Last Name" aria-required="true" class="form-row form-row-first">
+          </div>
+          <div class="col-md-4">
+            <input type="text" name="email" value="" size="40" placeholder="Booking Number" aria-required="true" class="form-row form-row-first">
+          </div>
+          <div class="col-md-12">
+            <input type="text" name="password" value="" size="40" placeholder="Title of your review" aria-required="true" class="form-row form-row-last">
+          </div>
+          <div class="col-md-12">
+            <textarea name="message" cols="40" rows="4" placeholder="Message of your review" aria-invalid="false" aria-required="true" class="mb-20"></textarea>
+            <input type="submit" value="Add a review" class="cws-button alt float-right">
+          </div>
+        </div>
+      </form>
 
-        @endsection
+      
+
+              
+    </div>
+  </div>
+  <!-- ! review -->
+  </section>
+</div>
+@endsection

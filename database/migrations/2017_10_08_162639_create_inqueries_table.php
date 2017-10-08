b0c8aef8dd801ec30d7c249bf8856573e31ba354
Yaniu09 <imperialaeiou@gmail.com>
2017-10-08 09:28:39 -0700

    yaniu

---
+++ database/migrations/2017_10_08_162639_create_inqueries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInqueriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inqueries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fname')->nullable();
+            $table->integer('lname')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('title')->nullable();
+            $table->longtext('inquiry')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inqueries');
+    }
+}

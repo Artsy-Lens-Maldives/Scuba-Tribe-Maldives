62f81fe880d9ad261c1906a1b148709fb1338c0f
athik13 <athik.13@gmail.com>
2017-10-08 21:11:42 +0500

    login little fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <div class="col-md-6">
+                                        <div class="col-md-6 col-md-offset-4">
                                             <div class="checkbox">
                                                 <label style="color: black;">
                                                     <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me

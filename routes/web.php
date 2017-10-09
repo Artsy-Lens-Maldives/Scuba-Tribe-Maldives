2531093fb128af8450a1dd5542b5b8c6844bf2a1
Yaniu09 <imperialaeiou@gmail.com>
2017-10-09 03:51:41 -0700

    contact us

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact-us', function () {
+    return view('contact-us');
+});
+
+
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 

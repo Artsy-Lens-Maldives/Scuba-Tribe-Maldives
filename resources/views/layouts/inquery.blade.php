b92200affcccfac16bf6673814659d91982fa8d3
athik13 <athik.13@gmail.com>
2017-10-10 17:24:59 +0500

    inqury jivo chat

--- resources/views/layouts/inquery.blade.php
+++ resources/views/layouts/inquery.blade.php
@@ -48,7 +48,7 @@
     </script>
     <!-- BEGIN JIVOSITE CODE {literal} -->
     <script type='text/javascript'>
-    (function(){ var widget_id = 'FAWE6vAwZg';var d=document;var w=window;function l(){
+    (function(){ var widget_id = 'GCwnGe0HuC';var d=document;var w=window;function l(){
     var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
     <!-- {/literal} END JIVOSITE CODE -->
     @yield('js')

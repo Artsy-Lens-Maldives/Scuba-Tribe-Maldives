70d0136cd0c9ab7eb082337852415b6c84ef1dac
athik13 <athik.13@gmail.com>
2017-10-11 17:24:21 +0500

    admin route /

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,15 @@
 <?php
 
+Route::get('/', function () {
+    $users[] = Auth::user();
+    $users[] = Auth::guard()->user();
+    $users[] = Auth::guard('admin')->user();
+
+    //dd($users);
+
+    return view('admin.home');
+});
+
 Route::get('/home', function () {
     $users[] = Auth::user();
     $users[] = Auth::guard()->user();

89b89117bb5abc0a4e40babb2eabfcdbcfe75add
athik13 <athik.13@gmail.com>
2017-11-21 18:55:52 +0500

    fixed catamaran layout

--- resources/views/catamaran/detail.blade.php
+++ resources/views/catamaran/detail.blade.php
@@ -16,27 +16,27 @@
         </div>
         <div class="container" style="">
           <div class="row">
-            <div class="col-md-7">
+            <div class="col-md-12">
               <div id="flex-slider" class="flexslider">
                 <ul class="slides">
                   @foreach($catamaran->images as $image)
-                  <li><img src="{{ $image->photo_url }}" alt></li>
+                    <li><img src="{{ $image->photo_url }}" alt></li>
                   @endforeach
                 </ul>
               </div>
               <div id="flex-carousel" class="flexslider">
                 <ul class="slides">
                   @foreach($catamaran->images as $image)
-                  <li><img src="{{ $image->photo_url }}" data-at2x="{{ $image->photo_url }}" alt></li>
+                    <li><img src="{{ $image->photo_url }}" data-at2x="{{ $image->photo_url }}" alt></li>
                   @endforeach
                 </ul>
               </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-12">
               <div class="bg-gray-3 p-30-40">
                 <h4>Boat Features</h4>
                 <div class="list-style-square">
-                {!! $catamaran->features !!}
+                  {!! $catamaran->features !!}
                 </div>
               </div>
             </div>

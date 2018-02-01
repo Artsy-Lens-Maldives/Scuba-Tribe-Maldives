7254194eb77dfcaf4994d7bf095a0222728f747d
athik13 <athik.13@gmail.com>
2018-02-01 11:09:09 +0500

    search start

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,5 +1,6 @@
 <form method="GET" class="form" action="/search">
     <div class="search-wrap">
-        <input name="q" type="text" placeholder="Where will you go next?" class="form-control search-field"><i class="flaticon-suntour-search search-icon"></i>
+        <input name="q" type="text" placeholder="Where will you go next?" class="submit_on_enter form-control search-field"><i class="flaticon-suntour-search search-icon"></i>        
+        <input type="submit" style="visibility: hidden;" />
     </div>
 </form>
